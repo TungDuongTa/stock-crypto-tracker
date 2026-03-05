e38d95e1d5f3aba950cdff702555ac7b5bc1e6a3
Tung Duong Ta <duongtatung@gmail.com>
2026-03-05 17:31:54 +1100

    try to fix .,,all route

--- app/api/[...all]/route.ts
+++ app/api/[...all]/route.ts
@@ -1,5 +1,4 @@
 import { auth } from "@/lib/better-auth/auth";
 import { toNextJsHandler } from "better-auth/next-js";
-
-export const { POST, GET } = toNextJsHandler(auth);
 export const dynamic = "force-dynamic";
+export const { POST, GET } = toNextJsHandler(auth);
